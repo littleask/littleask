832834ee0feb93263e15a7ca134923c5a2e9dcf4
frank <frank@webdev.(none)>
2012-04-01 15:33:21 -0400

    littlask0401 commit

--- littleask/littleask/settings.py
+++ littleask/littleask/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'littleask',                      # Or path to database file if using sqlite3.
         'USER': 'frank',                      # Not used with sqlite3.
-        'PASSWORD': 'dre805',                  # Not used with sqlite3.
+        'PASSWORD': '111',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
---
+++ littleask/website/admin.py
@@ -0,0 +1,4 @@
+from website.models import front_user, back_user
+from django.contrib import admin
+
+admin.site.register(front_user)
